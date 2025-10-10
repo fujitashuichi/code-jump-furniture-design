2d6e18d75e175e4424727200d405cd0be887dc68
fujitashuichi <fujitashuu14@gmail.com>
2025-10-10 17:34:53 +0900

    Add: itemページに商品情報を追加

--- js/itemPage.js
+++ js/itemPage.js
@@ -9,11 +9,32 @@ const item = products[itemId];
 
 document.title = item.name;
 
-const text =　`
+
+const strings = item.texts.map(string =>
+    `<p>${string}</p>`
+);
+
+let details = [];
+for(const key in item.details) {
+    details.push(`
+            <dt>${key}</dt>
+            <dd>${item.details[key]}</dd>
+        `);
+}
+
+const text = `
+    <h2 class="item-title">${item.name}</h2>
     <div class="img-wrapper">
         <img src="${item.img}"></img>
     </div>
+    <div class="text-wrapper">
+        ${strings.join("\n")}
+        <dl>
+            ${details.join("\n")}
+        </dl>
+    </div>
 `
+
 const parser = new DOMParser();
 const element = parser.parseFromString(text, "text/html");
 
